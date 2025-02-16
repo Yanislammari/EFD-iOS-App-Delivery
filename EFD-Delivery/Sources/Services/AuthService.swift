11c4a07267351e6b3880981b3ec8b60351a0229d
yanislammari <yanis.lammari937@gmail.com>
2025-02-16 04:15:11 +0100

    add decode token service

--- EFD-Delivery/Sources/Services/AuthService.swift
+++ EFD-Delivery/Sources/Services/AuthService.swift
@@ -11,6 +11,10 @@ class AuthService {
     private static var instance: AuthService!
     var message: String? = nil
     var token: String? = nil
+    
+    var api: API {
+        return API.getInstance()
+    }
         
     static func getInstance() -> AuthService {
         if self.instance == nil {
@@ -25,7 +29,7 @@ class AuthService {
             "password": password
         ]
         
-        let request = API.getInstance().request(route: "/deliveryman/login", method: "POST", body: body)
+        let request = api.request(route: "/deliveryman/login", method: "POST", body: body)
         let task: URLSessionDataTask = URLSession.shared.dataTask(with: request) { data, response, error in
             guard error == nil else {
                 return
@@ -33,7 +37,7 @@ class AuthService {
             guard let data = data else {
                 return
             }
-            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : Any] else {
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
                 return
             }
             DispatchQueue.main.async {
@@ -44,6 +48,26 @@ class AuthService {
                     self.token = token
                 }
                 completion()
+            }
+        }
+        task.resume()
+    }
+    
+    func decodeToken(token: String, completion: @escaping (String) -> Void) -> Void {
+        let request = api.request(route: "/decode-token", method: "GET", token: token)
+        let task: URLSessionDataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: String] else {
+                return
+            }
+            let userId = json["userId"]
+            DispatchQueue.main.async {
+                completion(userId!)
             }
         }
         task.resume()
